dc73b9e5eeab6063b2f45224cbebfad87888e6c9
Amit Malewar <ami.malewar@gmail.com>
2020-03-31 17:56:39 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+// Load Essential Fonts Awesome
+
+add_filter( 'infophilic_fontawesome_essentials', 'infophilic_fontawesome_essentials' );
+function infophilic_fontawesome_essentials()
+{
+   return true;
+}
